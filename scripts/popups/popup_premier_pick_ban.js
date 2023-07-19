26b891eed9bbf13f4e86a724e4526799f8adb909
StefanX <60297289+stefanx111@users.noreply.github.com>
2023-07-19 03:12:14 +0300

    2000081/13900

---
+++ scripts/popups/popup_premier_pick_ban.js
@@ -0,0 +1,7 @@
+/// <reference path="../csgo.d.ts" />
+/// <reference path="../util_gamemodeflags.ts" />
+/// <reference path="../common/formattext.ts" />
+/// <reference path="../common/sessionutil.ts" />
+/// <reference path="../common/teamcolor.ts" />
+/// <reference path="../avatar.ts" />
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicG9wdXBfcHJlbWllcl9waWNrX2Jhbi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInBvcHVwX3ByZW1pZXJfcGlja19iYW4udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEscUNBQXFDO0FBQ3JDLGlEQUFpRDtBQUNqRCxnREFBZ0Q7QUFDaEQsaURBQWlEO0FBQ2pELCtDQUErQztBQUMvQyxxQ0FBcUMifQ==
